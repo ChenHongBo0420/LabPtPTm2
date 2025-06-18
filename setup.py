ade9bacfd78164e7b65d2caf81259b2f4c664b38
JushuaC <1047280552@qq.com>
2025-06-18 22:30:20 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,13 @@
 
 from setuptools import setup, find_packages
 
-setup(name='labptptm2',
+setup(
+    name='labptptm2',
     version='0.2.1',
     packages=find_packages(),
     install_requires=[
-        'zarr[jupyter]==2.9.5',
+        'zarr[jupyter]==2.10.3',
+        'numcodecs==0.10.2',
         'fsspec',
         's3fs',
         'pyyaml'
